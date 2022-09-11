b9d6a09b63edc12ab7d3dcaa096773fd7e3e0291
Sara Fisher <sarrfish@gmail.com>
2022-09-11 18:48:20 -0500

    first draft of assignment 1

---
+++ script.js
@@ -0,0 +1,11 @@
+debugger;
+
+let num = 17;
+num += 2;
+let string1 = 'What ';
+let string2 = 'is your favorite number?';
+console.log(string1 + string2);
+document.getElementById("paragrapha").innerHTML = string1 + string2;
+document.getElementById("paragraphb").innerHTML = num;
+
+
